accca6694407803e25e6deab07ee0e4ba184864d
Nick Gromyko <nxgr.dev@gmail.com>
2024-02-14 15:48:24 +0600

    Add CRUD operations to Organization Service and Controller

--- src/controllers/OrganizationController.js
+++ src/controllers/OrganizationController.js
@@ -1,6 +1,7 @@
 const router = require("express").Router(),
     catchError = require("../../utils/catchError"),
     service = require("./OrganizationService")
+const ServiceError = require("../../utils/ServiceError")
 
 router.post("/organization", catchError(async (req, res, next) => {
     const organizationData = req.body
@@ -8,6 +9,48 @@ router.post("/organization", catchError(async (req, res, next) => {
     const result = await service.createOrganization({...organizationData, ownerId: req.user.id})
 
     res.status(200).send(result)
+}))
+
+
+router.put("/organization/:id", catchError(async (req, res, next) => {
+    const organizationData = req.body
+    let orgId = req.params.id
+
+    orgId = Number(orgId)
+
+    if (isNaN(orgId)) {
+        throw new ServiceError(400, "invalid-organization-id")
+    }
+
+    const ownerId = req.user.id
+
+    const result = await service.updateOrganization({id: orgId, ...organizationData, ownerId})
+
+    res.status(200).send(result)
+}))
+
+router.get("/organization/:id", catchError(async (req, res, next) => {
+    const orgId = req.params.id
+
+    res.status(200).send(await service.findOrganization(orgId, req.user.id))
+}))
+
+router.get("/organizations", catchError(async (req, res, next) => {
+    res.status(200).send(await service.findAllOrganizations(req.user.id))
+}))
+
+router.delete("/organization/:id", catchError(async (req, res, next) => {
+    let orgId = req.params.id
+
+    orgId = Number(orgId)
+
+    if (isNaN(orgId)) {
+        throw new ServiceError(400, "invalid-organization-id")
+    }
+
+    await service.deleteOrganization(orgId, req.user.id)
+
+    res.status(200).send()
 }))
 
 module.exports = router
--- src/controllers/OrganizationService.js
+++ src/controllers/OrganizationService.js
@@ -10,6 +10,12 @@ const OrganizationService = {
             throw new ServiceError(400, "incorrect-email")
         }
 
+        const user = await UserModel.findByPk(ownerId)
+
+        if (!user) {
+            throw new ServiceError(401, "user-not-found")
+        }
+
         const existingOrganization = await OrganizationModel.findOne({ where: { email } })
 
         if (existingOrganization) {
@@ -27,8 +33,70 @@ const OrganizationService = {
         }).catch(err => {
             throw new ServiceError(500, err)
         })
-    }
+    },
 
+    findOrganization: async (id, ownerId) => {
+        const organization = await OrganizationModel.findOne({ where: {id, owner: ownerId} })
+
+        if (!organization) {
+            throw new ServiceError(404, "organization-not-found")
+        }
+
+        return organization
+    },
+
+    findAllOrganizations: ownerId => {
+        return OrganizationModel.findAll({ where: {owner: ownerId} })
+            .then(organizations => organizations)
+            .catch(err => {
+                throw new ServiceError(500, err)
+            })
+    },
+
+    updateOrganization: async ({ id, name, email, description, address, ownerId }) => {
+        let organization = await OrganizationModel.findOne({ where: {id, owner: ownerId} })
+
+        if (!organization) {
+            throw new ServiceError(404, "organization-not-found")
+        }
+
+        if (email) {
+            if (!EMAIL_REGEX.test(email)) {
+                throw new ServiceError(400, "incorrect-email")
+            }
+
+            const existingOrganization = await OrganizationModel.findOne({ where: { email } })
+
+            if (existingOrganization && +existingOrganization.id !== id) {
+                throw new ServiceError(400, "email-taken")
+            }
+        }
+
+        return OrganizationModel.update({
+            name: name || organization.name,
+            email: email || organization.email,
+            description: description || organization.description,
+            address: address || organization.address,
+        }, {
+            where: { id: organization.id }
+        }).then(() => {
+            return OrganizationModel.findByPk(id)
+        }).catch(err => {
+            throw new ServiceError(500, err)
+        })
+    },
+    deleteOrganization: async (id, ownerId) => {
+        const organization = await OrganizationModel.findOne({ where: {id, owner: ownerId} })
+
+        if (!organization) {
+            throw new ServiceError(404, "organization-not-found")
+        }
+
+        return OrganizationModel.destroy({ where: { id } })
+            .catch(err => {
+                throw new ServiceError(500, err)
+            })
+    }
 }
 
 module.exports = OrganizationService
